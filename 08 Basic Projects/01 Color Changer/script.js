d1078a7321e4f420a78631fa021dd4f2aef7db4d
kunjshah4u <kunj08784@gmail.com>
2024-01-09 16:20:40 +0530

    Made BMI ccalulator and local digital clock

--- 08 Basic Projects/01 Color Changer/script.js
+++ 08 Basic Projects/01 Color Changer/script.js
@@ -7,20 +7,25 @@ buttons.forEach( (button) => {
     button.addEventListener('click', (e) => {
         // console.log(e);
         // console.log(e.target);
-        if(e.target.id === 'grey'){
-            body.style.backgroundColor = e.target.id
-        }
-        if(e.target.id === 'white'){
-            body.style.backgroundColor = e.target.id
-        }
-        if(e.target.id === 'blue'){
-            body.style.backgroundColor = e.target.id
-        }
-        if(e.target.id === 'yellow'){
-            body.style.backgroundColor = e.target.id
-        }
-        if(e.target.id === 'purple'){
-            body.style.backgroundColor = e.target.id
-        }
+
+        // solution
+        // if(e.target.id === 'grey'){
+        //     body.style.backgroundColor = e.target.id
+        // }
+        // if(e.target.id === 'white'){
+        //     body.style.backgroundColor = e.target.id
+        // }
+        // if(e.target.id === 'blue'){
+        //     body.style.backgroundColor = e.target.id
+        // }
+        // if(e.target.id === 'yellow'){
+        //     body.style.backgroundColor = e.target.id
+        // }
+        // if(e.target.id === 'purple'){
+        //     body.style.backgroundColor = e.target.id
+        // }
+
+        // some better approach
+        body.style.backgroundColor = e.target.id;
     })
 })
