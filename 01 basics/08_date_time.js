74cfb035c61e65d0b064ad06d050589210fc7737
kunjshah4u <kunj08784@gmail.com>
2024-02-09 17:09:35 +0530

    minor improvements in code and how formatting works in date

--- 01 basics/08_date_time.js
+++ 01 basics/08_date_time.js
@@ -6,6 +6,9 @@ console.log(myDate.toDateString());
 console.log(myDate.toLocaleString());
 console.log(typeof myDate);
 
+const formattedDate = myDate.toJSON().slice(0,10).split('-').reverse().join('-'); 
+console.log(formattedDate);
+
 // let myCreatedDate = new Date(2023, 0, 23)
 // let myCreatedDate = new Date(2023, 0, 23, 5, 3)
 // let myCreatedDate = new Date("2023-01-14")
@@ -14,14 +17,14 @@ console.log(myCreatedDate.toLocaleString());
 
 let myTimeStamp = Date.now()
 
-console.log(myTimeStamp);
+console.log(myTimeStamp); // returns the number of milliseconds since January 1, 1970.
 console.log(myCreatedDate.getTime());
 console.log(Math.floor(Date.now()/1000));
 
 let newDate = new Date()
 console.log(newDate);
 console.log(newDate.getMonth() + 1);
-console.log(newDate.getDay());
+console.log(newDate.getDay()); // returns the day of the week for this date according to local time, where 0 represents Sunday
 
 // `${newDate.getDay()} and the time `
 
@@ -30,4 +33,4 @@ console.log(newDate.toLocaleString('default', {
     // weekday: "long",
     dateStyle: "short", 
     // explore more options bt clicking Ctrl + Space
-}));
+}).replaceAll("/", "."));
