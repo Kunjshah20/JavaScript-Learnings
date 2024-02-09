325ee9ba0cf9924b380e82df57bfe2a57e2fb530
kunjshah4u <kunj08784@gmail.com>
2024-02-09 17:28:59 +0530

    added comments for code readability

--- 02 basics/01_array.js
+++ 02 basics/01_array.js
@@ -29,12 +29,12 @@ const myArr2 = new Array(1, 2, 3, 4)
 
 console.log("A ", myArr);
 
-const myn1 = myArr.slice(1, 3)
+const myn1 = myArr.slice(1, 3) // last index i.e 3 is not considered
 
 console.log(myn1);
 console.log("B ", myArr);
 
 
-const myn2 = myArr.splice(1, 3)
+const myn2 = myArr.splice(1, 3) // last index i.e 3 is considered
 console.log("C ", myArr);
 console.log(myn2);
